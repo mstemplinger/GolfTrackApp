9d1f37ddad1b7a5fb10db8439180ed733d0bcfbf
mstemplinger <154726904+mstemplinger@users.noreply.github.com>
2026-04-30 18:04:57 +0200

    Initial Commit

---
+++ GolfTrackApp/GolfTrackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfTrackAppApp.swift
+//  GolfTrackApp
+//
+//  Created by Tobias Aufschläger on 30.04.26.
+//
+
+import SwiftUI
+
+@main
+struct GolfTrackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GolfTrackAppTests/GolfTrackAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  GolfTrackAppTests.swift
+//  GolfTrackAppTests
+//
+//  Created by Tobias Aufschläger on 30.04.26.
+//
+
+import Testing
+@testable import GolfTrackApp
+
+struct GolfTrackAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
